c50a771031c531dbdcccd09707d4f3896e2c2b9d
bernardocaputo <bernardocaputo@gmail.com>
2017-05-17 22:36:19 -0300

    creating models

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'sinatra/activerecord'
 
 require './config/database'
 
+# Load Models => poderia fazer um require './app/models/blabla.rb' para cada model mas assim automatiza
+Dir["./app/models/*.rb"].each {|file| require file }
+
 class App < Sinatra::Base # é isso que coloca o sinatra no projeto
   get '/' do
     'Hello world!'
